dba1ec0482eb64d063fb7d99934b31d1c4715142
ichwalM <13020230049@student.umi.ac.id>
2025-05-03 22:14:14 +0800

    OOP-4: Generic Class & Generic Method

--- tugas_eval_5/Main.java
+++ tugas_eval_5/Main.java
@@ -3,7 +3,7 @@ public class Main {
         KotakGenerik<Integer> kotak1 = new KotakGenerik<>(100);
         KotakGenerik<Integer> kotak2 = new KotakGenerik<>(100);
         KotakGenerik<String> kotak3 = new KotakGenerik<>("100");
-
+        //pemanggilan method untuk menampilkan isi kotak
         System.out.println("Kotak1 == Kotak2: " + kotak1.samaDengan(kotak2));
         System.out.println("Kotak1 == Kotak3: " + kotak1.samaDengan(kotak3));
     }
